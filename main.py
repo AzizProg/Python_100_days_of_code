8af17861ea02d644e24266503849a3c1551b32cb
AzizProg <112016586+AzizProg@users.noreply.github.com>
2023-10-07 21:47:02 +0000

    Taking user input

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Getting to know you!")
+
+YourName = input("What is your name?")
+Hungry = input ("What is your favorite food?")
+Music = input("What is your favorite music?")
+WhereAreYou = input("Where are you?")
+
+print(f"You are {YourName} You're probably hungry for {Hungry}. You're probably listening to {Music}. You're probably living in the amazing. You're probably living in the amazing {WhereAreYou}, Have a great day!")
