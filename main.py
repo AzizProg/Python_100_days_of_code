eae2f59ea68b6ea69286f823197271db4d26f9a0
AzizProg <112016586+AzizProg@users.noreply.github.com>
2023-10-07 21:27:54 +0000

    print-color

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Uh, oh, you've been given a", "\033[31m", "warning", "\033[0m", "for being a bad, bad person.")
+
+name:str=input("What's your name? : ")
+superpower:str=input("Your superpower : ")
+manga:str=input("What's manga do you prefer? : ")
+live:str=input("Where do you live? : ")
+
+print(f"Welcome \033[32m{name}\033[0m, you have the young that like \033[33m{manga}\033[0m and live in \033[34m{live}\033[0m. I like your superpower:\033[31m{superpower}")
