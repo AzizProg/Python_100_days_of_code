594ecdd6c8edb4afcd37e671c312f42f3dde0c22
AzizProg <112016586+AzizProg@users.noreply.github.com>
2023-10-08 10:16:03 +0000

    challenge if else

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-myName = input("What's your name?: ")
-if (myName == "David"):
-  print("Welcome Dude!")
+manga:str=input("Do you prefer Jujutsu Kaisen or Naruto : ").lower()
+  
+if manga=="jujutsu kaisen":
+  print(f"\nYou prefer \033[32m{manga}\033[0m, you are in the present.")
+elif manga=="naruto":
+  print(f"\nYou like \033[32m{manga}\033[0m, you are a nostagic.")
+else:
+  print(f"I don't know this \033[33m{manga}\033[0m but it must to be fun!")
