554c8d983d87e65a86ff2c3d904e82c2fe9b8ae8
AzizProg <traoreabdoulaziz70@gmail.com>
2023-10-13 11:56:17 +0000

    challenge terminated

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("test commit")
+print("Welcome in the jungle ")
+choice:str=input("Manga and Anime, what do you prefer?").lower()
+if choice=="manga":
+    print("Nice , you a right fan")
+    manga:str=input("Manga's name :").lower()
+    if manga=="jujutsu kaisen" or "naruto" or "one piece" or "dragon ball":
+      print("You an old fan")
+    else:
+      print(f"I don't know {manga} but it must to be fun ! ")
+elif choice=="anime":
+  print("Gooood! you must to like animations")
+  anime:str=input("Your anime name").lower()
+  if anime=="jujutsu kaisen" or "naruto" or "one piece" or "dragon ball":
+    print("You an old fan")
+  else:
+    print(f"I don't know {anime} but it must to be fun ! ")
+else:
+  print("I see that you don't like anime or manga")
